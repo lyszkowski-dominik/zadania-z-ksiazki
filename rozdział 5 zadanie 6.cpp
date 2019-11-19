ce0a2d14a8a50ede9993262f20db08c3a9df1e4b
lyszkowski-dominik <lyszkowskidominik@gmail.com>
2019-11-19 19:33:15 +0100

    cały program

--- rozdział 5 zadanie 6.cpp
+++ rozdział 5 zadanie 6.cpp
@@ -4,5 +4,24 @@ int main() {
 	using namespace std;
 	setlocale(LC_ALL, "pl_PL");
 
+	struct car {
+		string brand;
+		int yearOfProduction;
+	};
+	cout << "Ile samochodów chcesz skatalogowaæ ?" << endl;
+	int howMany;
+	cin >> howMany;
+	car* cars = new car[howMany];
+	for (i = 0; i < howMany; i++)
+	{
+		cout << "Samochód #"<<i+1<<endl<<"Podaj markê samochodu: " << endl;
+		cin >> car[i].brand;
+		cout << "Podaj rok produkcji: " << endl;
+		cin >> car[i].yearOfProduction;
+	}
+	for (i = 0; i < howMany; i++)
+	{
+		cout << car[i].yearOfProduction << " " << car[i].brand;
+	}
 	return 0;
 }
