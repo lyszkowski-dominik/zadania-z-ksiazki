ec7931985f19db446eb79fcd6c5957f7d7aeebe0
lyszkowski-dominik <lyszkowskidominik@gmail.com>
2020-02-19 20:23:08 +0100

    zadanie 9 rozdział 6

---
+++ zadanie 9 rozdział 6/Źródło.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <fstream>
+using namespace std;
+struct donors { char name[50]; char surname[50]; double datek; };
+int main() {
+	
+	setlocale(LC_ALL, "pl_PL");
+
+	
+	string filename;
+	ifstream inFile;
+	cout << "Podaj nazwê pliku: \n";
+	cin >> filename;
+	inFile.open(filename);
+	int users;
+	inFile >> users;
+	donors* ksiega = new donors[users];
+
+	string surname;
+
+	double price;
+	for (int i = 0; i < users; i++)
+	{
+		inFile >> ksiega[i].name >> ksiega[i].surname >> ksiega[i].datek;
+	}
+
+
+
+
+	cout << "Nasi Wspaniali Sponsorzy \n";
+
+	for (int i = 0; i < users; i++)
+	{
+		if (ksiega[i].datek >= 10000)
+		{
+			cout << ksiega[i].name << " " << ksiega[i].surname << " " << ksiega[i].datek << endl;
+		}
+	}
+	cout << "Nasi Sponsorzy \n";
+	for (int i = 0; i < users; i++)
+	{
+		if (ksiega[i].datek < 10000)
+			cout << ksiega[i].name << " " << ksiega[i].surname << " " << ksiega[i].datek << endl;
+	}
+	delete[]ksiega;
+
+	return 0;
+}
